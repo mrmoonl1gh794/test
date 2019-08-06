22454e5d70390139f0089942400fc3dc7931a28f
test <test@localhost.localdomain>
2019-08-06 13:00:13 -0400

    some init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class script {
-	file { '/etc/example_repo/test/script-1.0.sh':
-	ensure	=>	present,
+	package { '/etc/example_repo/test/script-1.0.sh':
+	ensure	=>	installed,
 	owner	=>	'root',
 	mode	=>	'0755',
 	content	=>	template('template.erb')
